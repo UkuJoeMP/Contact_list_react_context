6d712497f61d478bd816e2730ac16d4c8ebd3f36
UkuJoeMP <josemipinton@gmail.com>
2025-02-15 01:41:12 +0000

    Fist commit

--- src/js/layout.js
+++ src/js/layout.js
@@ -10,6 +10,10 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
+import Contacts from "./views/Contacts.jsx";
+import AddContact from "./views/AddContact.jsx";
+import EditContact from "./views/EditContact.jsx";
+
 //create your first component
 const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
@@ -19,16 +23,14 @@ const Layout = () => {
 	return (
 		<div>
 			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
+				<Routes>
+					{/* <Route path="/" element={<Home />} /> */}
+					<Route path="/" element={<Contacts />} />
+					<Route path="/addContact" element={<AddContact />} />
+					<Route path="/editContact/:id" element={<AddContact />} />
+					<Route path="*" element={<h1>Not found!</h1>} />
+
+				</Routes>
 			</BrowserRouter>
 		</div>
 	);
